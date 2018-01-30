a9d7d082f97f82dce5490912fe2e9f89b86068fa
ppanigrahi02 <padarabinda.panigrahi@cognizant.com>
2018-01-30 14:26:54 +0530

    initial

---
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+	
+	@GetMapping("/checkzuul")
+	public String hello() {
+		return "zuul working";
+	}
+	
+	@GetMapping("/checkerror")
+	public String error() throws Exception {
+		throw new Exception("*********ERROR************");
+	}
+}
